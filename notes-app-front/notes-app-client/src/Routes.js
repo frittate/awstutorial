a49b61605652a811aa484b089238090607a3812d
Sebastian Martin <acedia247@gmail.com>
2017-06-05 20:28:09 +0200

    http://serverless-stack.com/chapters/signup-with-aws-cognito.html

--- notes-app-front/notes-app-client/src/Routes.js
+++ notes-app-front/notes-app-client/src/Routes.js
@@ -7,11 +7,14 @@ import Login from './containers/Login';
 import AppliedRoute from './components/AppliedRoute';
 import NewNote from './containers/NewNote';
 
+import Signup from './containers/Signup';
+
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
